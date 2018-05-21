b217803a9cab9d7b45f1fc49a1c3f371ad81ec5d
qingxp9 <qingxp9@gmail.com>
2018-05-21 21:48:48 +0800

    update install

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 # This mirror for Nethunter 3.0
 # echo "deb http://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib" > /etc/apt/sources.list
 apt update
-apt install -y pppoe tshark 
+apt install -y pppoe tshark
+
 touch /etc/ppp/pppoe-server-options
-echo -e "require-pap\nlcp-echo-interval 10\nlcp-echo-failure 2" > /etc/ppp/pppoe-server-options
+echo "require-pap" > /etc/ppp/pppoe-server-options
+echo "lcp-echo-interval 10" >> /etc/ppp/pppoe-server-options
+echo "lcp-echo-failure 2" >> /etc/ppp/pppoe-server-options
+
 echo "* * * *" > /etc/ppp/pap-secrets
